04f2c263a0d8432235cd04158918381b50bfb1cf
Stephen <stvlley.amzn@gmail.com>
2022-07-08 18:16:05 -0400

    i found my fucking calling

--- src/pages/components/News.js
+++ src/pages/components/News.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const News = () => {
     return (
-        <div className="hover:scale-105 cursor-pointer duration-500 flex flex-col justify-center items-center text-center rounded shadow-xl border-2 border-gray-500 h-full w-full p-6">
-            <h2 className="text-lg text-gray-700">News</h2>
+        <div className="hover:scale-105 cursor-pointer duration-500 flex flex-col justify-center items-center text-center rounded  h-full w-full p-6">
+            <h2 className="text-lg text-gr">News</h2>
         </div>
     )
 }
--- src/pages/components/Notes.js
+++ src/pages/components/Notes.js
@@ -1,17 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {AiOutlinePlusCircle} from 'react-icons/Ai'
-const Notes = () => {
+import Modal from 'react-modal'
 
+const Notes = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false)
     const handleNewEntryClick = () => {
-        console.log('hello')
+        setModalIsOpen(true)
     }
     
     return (
-        <div className="hover:scale-105  duration-500 flex flex-col justify-center items-center text-center rounded   h-full w-full p-6">
+        <div  className="hover:scale-105  duration-500 flex flex-col justify-center items-center text-center rounded   h-full w-full p-6">
             <div onClick={handleNewEntryClick}>
             <h2 className="text-lg text-gray-500 p-4 cursor-pointer">New Entry</h2>
             <button className='text-4xl'><AiOutlinePlusCircle /></button>
             </div>
+            <Modal 
+                style={{
+                    overlay: {
+                      position: 'fixed',
+                      top: 100,
+                      left: 100,
+                      right: 100,
+                      bottom: 100,
+                      opacity: '90%',
+                      backgroundColor: '#34d8eb',
+                      borderRadius: '20px',
+                      maxHeight: 350
+                    },
+                    content: {
+                      position: 'absolute',
+                      top: '40px',
+                      left: '40px',
+                      right: '40px',
+                      bottom: '40px',
+                      border: '1px solid #ccc',
+                      background: '#fff',
+                      overflow: 'auto',
+                      WebkitOverflowScrolling: 'touch',
+                      borderRadius: '20px',
+                      outline: 'none',
+                      padding: '20px'
+                    }
+                  }}
+                isOpen={modalIsOpen} ariaHideApp={false}>
+                <div >
+                  <span id='top-of-modal'>
+                  <h2 className='text-3xl text-gray-500'>New Entry</h2>
+                <button className='p-3 rounded-lg bg-[#d15660] m-3 text-gray-200' onClick={() => setModalIsOpen(false)}>Close</button>
+
+                  </span>
+                <div >
+                    <textarea id='input-area' type='textarea' placeholder='...' />
+                </div>
+                <button className='text-gray-600 my-3 p-3 rounded-lg bg-[#34d8eb] '>Execute</button>
+                </div>
+            </Modal>
         </div> 
     )
 }
